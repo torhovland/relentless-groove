23ea34d2e583308dc7c2545adc509a1d15b5e899
Tor Hovland <tor.hovland@bekk.no>
2019-11-30 18:41:16 +0100

    VS Code devcontainer for Rust.

---
+++ Rust/src/main.rs
@@ -0,0 +1,10 @@
+/*--------------------------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+ *-------------------------------------------------------------------------------------------------------------*/
+
+fn main() {
+    let name = "VS Code Remote - Containers";
+    let foo = 5;
+    println!("Hello, {}!", name);
+}
